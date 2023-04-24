65754271e9f765afa7edbb557ca47bb7a24ad80f
Mitchel Walker <mitchelwalks@gmail.com>
2023-04-24 16:42:57 -0400

    Cleaned up code and added buffer-overflow protections.

--- nano_33/src/main.cpp
+++ nano_33/src/main.cpp
@@ -6,31 +6,14 @@
 #include "wifi_config.hpp"
 #include <ArduinoJson.h>
 
-#define PIN_TRFR  21    // TRFR Pin of Skywriter
-#define PIN_RESET 20    // Reset Pin of Skywriter
-
-void gesture(unsigned char type);
-void touch(unsigned char type);
-void xyz(unsigned int x, unsigned int y, unsigned int z);
-void brightnessWheel(int deg);
-void onMqttMessage(int messageSize);
-void setLampHsv(float h, float s, float v);
-void setLampState();
-void publishState();
-
-// Config
-long touch_timeout = 0;
-unsigned int max_x, max_y, max_z;
-unsigned int min_x, min_y, min_z;
 
 // Pin constants
-const int adc_bitwidth = 256;
+const int dac_bitwidth = 256;
 const int red_led = 5;
 const int green_led = 6;
 const int blue_led = 4;
-
-bool delta_rst = true;
-int old_delta = 0;
+const int reset_pin = 20;  // TRFR Pin of Skywriter
+const int trfr_pin = 21;   // Reset Pin of Skywriter
 
 // State
 bool is_on = true;
@@ -43,22 +26,28 @@ int mode = 0;
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 const char broker[] = "ec2-107-22-75-242.compute-1.amazonaws.com";
-int        port     = 50001;
-const char pub_topic[]  = "devices/08b61f821470/lamp/changed";
-const char sub_topic[]  = "devices/08b61f821470/lamp/set_config";
+int port = 50001;
+const char pub_topic[] = "devices/08b61f821470/lamp/changed";
+const char sub_topic[] = "devices/08b61f821470/lamp/set_config";
 const char willTopic[] = "lamp/connection/08b61f821470/";
 String willPayload = "0";
 bool willRetain = true;
 int willQos = 2;
 int subscribeQos = 1;
 
+
+void gesture(unsigned char type);
+void brightnessWheel(int deg);
+void onMqttMessage(int messageSize);
+void setLampHsv(float h, float s, float v);
+void setLampState();
+void publishState();
+
+
 void setup() {
-    Skywriter.begin(PIN_TRFR, PIN_RESET);
+    Skywriter.begin(trfr_pin, reset_pin);
     Skywriter.onGesture(gesture);
-//    Skywriter.onTouch(touch);
-//    Skywriter.onXYZ(xyz);
     Skywriter.onAirwheel(brightnessWheel);
-//    analogWriteRes(12);
 
     pinMode(red_led, OUTPUT);
     pinMode(green_led, OUTPUT);
@@ -67,12 +56,12 @@ void setup() {
     Serial.begin(9600);
 //    while(!Serial);
 
+    // Set the initial state to red
     setLampHsv(hue, saturation, brightness);
 
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
     while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
-        // failed, retry
         Serial.print(".");
         delay(5000);
     }
@@ -98,86 +87,74 @@ void setup() {
 
     mqttClient.subscribe(sub_topic, subscribeQos);
 
+    // Send the initial state to the broker
     publishState();
 }
 
+
 void loop() {
     Skywriter.poll();
-    if( touch_timeout > 0 ) touch_timeout--;
     mqttClient.poll();
 }
 
-void gesture(unsigned char type){
+
+void gesture(unsigned char type) {
     Serial.println("Got gesture ");
     Serial.print(type,DEC);
     Serial.print('\n');
 
-    if (type == SW_FLICK_SOUTH_NORTH) {
+    if (type == SW_FLICK_SOUTH_NORTH) {         // Swipe Up
         is_on = true;
         setLampState();
-    } else if (type == SW_FLICK_NORTH_SOUTH) {
+    } else if (type == SW_FLICK_NORTH_SOUTH) {  // Swipe Down
         is_on = false;
         setLampState();
-    } else if (type == SW_FLICK_WEST_EAST) {
+    } else if (type == SW_FLICK_WEST_EAST) {    // Swipe Right
         mode = (mode + 1) % 3;
-    } else if (type == SW_FLICK_EAST_WEST) {
+    } else if (type == SW_FLICK_EAST_WEST) {    // Swipe Left
         mode = (mode - 1) % 3;
-    } else {
-        mode = 0;
     }
 }
 
 void brightnessWheel(int deg) {
-    double delta = (double)deg / 360.;
+    float delta = (float)deg / 360.;  // Angular change scaled 0-1
+    float coeff;                      // Coefficient to adjust the amount of change reflected in a given delta for each action
 
-    if (mode == 0) {
-        double coeff = 0.25;
+    if (mode == 0) {            // Hue Select
+        coeff = 0.25;
         hue += delta * coeff;
-        if (hue > 1)
+        if (hue > 1)       // Wrap hue back to 0 after exceeding 1
             hue = 0;
-        else if (hue < 0)
+        else if (hue < 0)  // And vice versa
             hue = 1;
-    } else if (mode == 1) {
-        double coeff = 0.35;
+    } else if (mode == 1) {     // Saturation Select
+        coeff = 0.35;
         saturation += delta * coeff;
+        // Bound Saturation from 0-1
         if (saturation > 1)
             saturation = 1;
         else if (saturation < 0)
             saturation = 0;
-    } else if (mode == 2) {
-        double coeff = 0.5;
+    } else if (mode == 2) {     // Brightness Select
+        coeff = 0.5;
         brightness += delta * coeff;
+        // Bound Brightness from 0-1
         if (brightness > 1)
             brightness = 1;
         else if (brightness < 0)
             brightness = 0;
-//        char buf[128];
-//        sprintf(buf, "Deg: %d, Delta: %d, Brightness: %d", deg, delta, brightness);
-//        Serial.println(buf);
     }
     setLampState();
 }
 
-void onMqttMessage(int messageSize) {
-    // we received a message, print out the topic and contents
-    Serial.print("Received a message with topic '");
-    Serial.print(mqttClient.messageTopic());
-    Serial.print("', duplicate = ");
-    Serial.print(mqttClient.messageDup() ? "true" : "false");
-    Serial.print(", QoS = ");
-    Serial.print(mqttClient.messageQoS());
-    Serial.print(", retained = ");
-    Serial.print(mqttClient.messageRetain() ? "true" : "false");
-    Serial.print("', length ");
-    Serial.print(messageSize);
-    Serial.println(" bytes:");
 
+void onMqttMessage(int messageSize) {
     char json[1024];
     int i = 0;
-    while (mqttClient.available()) {
+    while (mqttClient.available() && i < 1023) {
         char c = (char)mqttClient.read();
         Serial.print(c);
-        json[i++] = c;
+        json[i++] = c;  // Read characters into the json buffer
     }
     json[i] = 0;
 
@@ -192,13 +169,15 @@ void onMqttMessage(int messageSize) {
     setLampState();
 }
 
+
 void setLampHsv(float h, float s, float v) {
     float rgb[3];
     hsv2rgb(h, s, v, rgb);
-    analogWrite(red_led, (int)(rgb[0] * 255));
-    analogWrite(green_led, (int)(rgb[1] * 255));
-    analogWrite(blue_led, (int)(rgb[2] * 255));
+    analogWrite(red_led, (int)(rgb[0] * (dac_bitwidth - 1)));
+    analogWrite(green_led, (int)(rgb[1] * (dac_bitwidth - 1)));
+    analogWrite(blue_led, (int)(rgb[2] * (dac_bitwidth - 1)));
 }
+
 
 void setLampState() {
     if (is_on) {
@@ -212,8 +191,8 @@ void setLampState() {
 
 void publishState() {
     char json_out[1024];
-    sprintf(json_out, R"({"color": {"h": %f, "s": %f}, "brightness": %f, "on": %s, "client": "08b61f821470"})",
-            hue, saturation, brightness, is_on ? "true" : "false");
+    snprintf(json_out, 1024, R"({"color": {"h": %f, "s": %f}, "brightness": %f, "on": %s, "client": "08b61f821470"})",
+             hue, saturation, brightness, is_on ? "true" : "false");
 
     mqttClient.beginMessage(pub_topic);
     mqttClient.print(json_out);
